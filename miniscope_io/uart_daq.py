c5fb7ffaa7d91348959cc582f41e3cf1f1d4926b
t-sasatani <sasatani.dev@gmail.com>
2023-07-18 15:54:41 -0700

    fixed process termination

--- miniscope_io/uart_daq.py
+++ miniscope_io/uart_daq.py
@@ -8,15 +8,12 @@ import sys
 import time
 
 class uart_daq:
-    # COM port should probably be automatically found but not sure yet how to distinguish with other devices.
-    def __init__(self, frame_width = 304, frame_height = 304, preamble = b'\x34\x12', comport = 'COM3'):
+    def __init__(self, frame_width = 304, frame_height = 304, preamble = b'\x34\x12'):
         self.frame_width = frame_width
         self.frame_height = frame_height
-        self.num_pixel_assert = frame_width * frame_height
         self.preamble = preamble
-        self.comport = comport
 
-    def _uart_recv(self, serial_buffer_read):
+    def _uart_recv(self, serial_buffer_read, comport, baudrate):
         locallogs = logging.getLogger(__name__)
         locallogs.setLevel(logging.DEBUG)
 
@@ -28,7 +25,7 @@ class uart_daq:
         locallogs.addHandler(file)
         coloredlogs.install(level=logging.INFO, logger=locallogs)
 
-        serial_port = serial.Serial(self.comport, 1200000, timeout=5, stopbits=1)
+        serial_port = serial.Serial(port=comport, baudrate=baudrate, timeout=5, stopbits=1)
         locallogs.info('Serial port: ' + str(serial_port.name))
 
         for i in range(0,100): # how many buffers to see
@@ -103,6 +100,7 @@ class uart_daq:
         locallogs.addHandler(file)
         coloredlogs.install(level=logging.INFO, logger=locallogs)
 
+        # This shouldn't be hardcoded
         buffer_numpixel_assert = [20440, 20440, 20440, 20440, 10656]
 
         HEADER_LENGTH = (10 - 1) * 4
@@ -116,7 +114,7 @@ class uart_daq:
                 num_buffer_pixel = 0
                 lost_frame_pixel = 0
 
-                imagearray_copy = np.zeros(int(self.num_pixel_assert), np.uint8)
+                imagearray_copy = np.zeros(int(self.frame_width * self.frame_height), np.uint8)
 
                 for i in range(0,5):
                     #num_buffer_pixel = int.from_bytes(plot_buffer_copy[i][28:32][::-1], "big")
@@ -147,29 +145,18 @@ class uart_daq:
                             ', TOTAL_PX: ' + str(num_frame_pixel) + \
                             ', LOST_PX: ' + str(lost_frame_pixel))
                 
-                if len(pixel_vector) > self.num_pixel_assert:
-                    pixel_vector = pixel_vector[0:self.num_pixel_assert]
+                if len(pixel_vector) > self.frame_width * self.frame_height:
+                    pixel_vector = pixel_vector[0:self.frame_width * self.frame_height]
                 #if num_frame_pixel == self.num_pixel_assert:
-                if 1:
-                    imagearray_copy[0::4] = np.uint8(pixel_vector[3::4])
-                    imagearray_copy[1::4] = np.uint8(pixel_vector[2::4])
-                    imagearray_copy[2::4] = np.uint8(pixel_vector[1::4])
-                    imagearray_copy[3::4] = np.uint8(pixel_vector[0::4])
-                if 0:
-                    for pixel_index in range(0, num_frame_pixel):
-                        if pixel_index % 4 == 0:
-                            #imagearray_copy[pixel_index] = np.uint8(pixel_vector[pixel_index + 3])
-                            imagearray_copy[pixel_index] = np.uint8(pixel_vector[pixel_index + 3])
-                        elif pixel_index % 4 == 1:
-                            imagearray_copy[pixel_index] = np.uint8(pixel_vector[pixel_index + 1])
-                        elif pixel_index % 4 == 2:
-                            imagearray_copy[pixel_index] = np.uint8(pixel_vector[pixel_index - 1])
-                        elif pixel_index % 4 == 3:
-                            imagearray_copy[pixel_index] = np.uint8(pixel_vector[pixel_index - 3])
+                imagearray_copy[0::4] = np.uint8(pixel_vector[3::4])
+                imagearray_copy[1::4] = np.uint8(pixel_vector[2::4])
+                imagearray_copy[2::4] = np.uint8(pixel_vector[1::4])
+                imagearray_copy[3::4] = np.uint8(pixel_vector[0::4])
                 imagearray.put(imagearray_copy)
                 locallogs.info('FRAME: ' + str(int.from_bytes(plot_buffer_copy[0][4:8][::-1], "big")) + ' stored!')
 
-    def capture(self):
+    # COM port should probably be automatically found but not sure yet how to distinguish with other devices.
+    def capture(self, comport = 'COM3', baudrate = 1200000):
         file = logging.FileHandler(datetime.now().strftime('log/logfile%Y_%m_%d_%H_%M.log'))
         file.setLevel(logging.DEBUG)
         fileformat = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s",datefmt="%H:%M:%S")
@@ -181,12 +168,13 @@ class uart_daq:
         globallogs.addHandler(file)
         coloredlogs.install(level=logging.DEBUG, logger=globallogs)
 
+        #Queue size is hard coded
         serial_buffer_read = multiprocessing.Queue(10) # b'\x00' # hand over single buffer: uart_recv() -> buffer_to_frame()
         buffer_frame = multiprocessing.Queue(5) #[b'\x00', b'\x00', b'\x00', b'\x00', b'\x00'] # hand over a frame (five buffers): buffer_to_frame()
         imagearray = multiprocessing.Queue(5)
-        imagearray.put(np.zeros(int(self.num_pixel_assert), np.uint8))
+        imagearray.put(np.zeros(int(self.frame_width * self.frame_height), np.uint8))
 
-        p_uart_recv = multiprocessing.Process(target=self._uart_recv, args=(serial_buffer_read, ))
+        p_uart_recv = multiprocessing.Process(target=self._uart_recv, args=(serial_buffer_read, comport, baudrate))
         p_buffer_to_frame = multiprocessing.Process(target=self._buffer_to_frame, args=(serial_buffer_read, buffer_frame, ))
         p_format_frame = multiprocessing.Process(target=self._format_frame, args=(buffer_frame, imagearray, ))
         p_uart_recv.start()
@@ -205,3 +193,7 @@ class uart_daq:
                 cv2.waitKey(100)
                 break # esc to quit
         print('End capture')
+
+if __name__ == '__main__':
+    daq_inst = uart_daq()
+    daq_inst.capture(comport = 'COM3', baudrate = 1200000)
