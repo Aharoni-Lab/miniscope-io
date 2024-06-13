9c7797e348d2bd04def4391660c2e5735431d1ac
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-06-12 20:48:36 -0700

    move example configs into a data directory to avoid namespace clashes with an eventual `config` package

--- miniscope_io/__init__.py
+++ miniscope_io/__init__.py
@@ -1,3 +1,18 @@
+from pathlib import Path
+
 from miniscope_io.config import Config
 from miniscope_io.logging import init_logger
 from miniscope_io.io import SDCard
+
+BASE_DIR = Path(__file__).parent.resolve()
+DATA_DIR = BASE_DIR / 'data'
+CONFIG_DIR = DATA_DIR / 'config'
+
+__all__ = [
+    'BASE_DIR',
+    'DATA_DIR',
+    'CONFIG_DIR',
+    'Config',
+    'SDCard',
+    'init_logger',
+]
