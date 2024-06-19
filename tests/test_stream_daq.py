af28945a874913ca55da1bd2d9744742059545c4
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-06-18 21:03:27 -0700

    hash from gh

--- tests/test_stream_daq.py
+++ tests/test_stream_daq.py
@@ -13,7 +13,8 @@ from .mock.opalkelly import okDevMock
             'stream_daq_test_200px.yml',
             'stream_daq_test_fpga_raw_input_200px.bin',
             ('3c0b7caedc5f6506fdfdc4f1277ba76a3a911dd79e5240801455a74292f793f5',
-             'e55462cc06cf6082952b2ce0fdd956cf1408fb492cddb6fb797119235b559a93')
+             'e55462cc06cf6082952b2ce0fdd956cf1408fb492cddb6fb797119235b559a93',
+             '290fd85bb3d00f896e695564eedc072e5e1f2837c4a0f19282e80624bae56401')
         )
     ]
 )
