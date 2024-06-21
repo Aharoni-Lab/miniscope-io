4a84f767ecc23bec804a326e9782fec3b67d08c0
t-sasatani <33111879+t-sasatani@users.noreply.github.com>
2024-06-21 11:22:56 +0900

    generate reference hash in testcode

--- tests/test_stream_daq.py
+++ tests/test_stream_daq.py
@@ -7,22 +7,22 @@ from .conftest import DATA_DIR, CONFIG_DIR
 from .mock.opalkelly import okDevMock
 
 @pytest.mark.parametrize(
-    'config,data,hash',
+    'config,data,test_video',
     [
         (
             'stream_daq_test_200px.yml',
             'stream_daq_test_fpga_raw_input_200px.bin',
-            ('3c0b7caedc5f6506fdfdc4f1277ba76a3a911dd79e5240801455a74292f793f5',
-             'e55462cc06cf6082952b2ce0fdd956cf1408fb492cddb6fb797119235b559a93',
-             '290fd85bb3d00f896e695564eedc072e5e1f2837c4a0f19282e80624bae56401')
+            'stream_daq_test_output_200px.avi'
         )
     ]
 )
 @pytest.mark.timeout(30)
-def test_video_output(config, data, hash, tmpdir, monkeypatch):
+def test_video_output(config, data, test_video, tmpdir, monkeypatch):
     output_video = tmpdir / 'output.avi'
 
     test_config_path = CONFIG_DIR / config
+    test_video_path = DATA_DIR / test_video
+
     daqConfig = StreamDaqConfig.from_yaml(test_config_path)
 
     data_file = DATA_DIR / data
@@ -33,5 +33,9 @@ def test_video_output(config, data, hash, tmpdir, monkeypatch):
 
     assert output_video.exists()
 
+
     video_hash = hash_file(output_video)
-    assert video_hash in hash
+    test_video_hash = hash_file(test_video_path)
+
+    assert video_hash == test_video_hash
+    #assert video_hash in hash
