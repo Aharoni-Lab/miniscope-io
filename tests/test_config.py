8a3c90dc9fde9c93c76449437cc46c8a3e951c17
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-06-21 01:51:04 -0700

    apparently it's tmp_path now

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,29 +2,29 @@ import os
 from pathlib import Path
 from miniscope_io import Config
 
-def test_config(tmpdir):
+def test_config(tmp_path):
     """
     Config should be able to make directories and set sensible defaults
     """
-    config = Config(base_dir = tmpdir)
+    config = Config(base_dir = tmp_path)
     assert config.base_dir.exists()
     assert config.log_dir.exists()
     assert config.log_dir == config.base_dir / 'logs'
 
 
-def test_config_from_environment(tmpdir):
+def test_config_from_environment(tmp_path):
     """
     Setting environmental variables should set the config, including recursive models
     """
-    os.environ['MINISCOPE_IO_BASE_DIR'] = str(tmpdir)
+    os.environ['MINISCOPE_IO_BASE_DIR'] = str(tmp_path)
     # we can also override the default log dir name
-    override_logdir = Path(tmpdir) / 'fancylogdir'
+    override_logdir = Path(tmp_path) / 'fancylogdir'
     os.environ['MINISCOPE_IO_LOG_DIR'] = str(override_logdir)
     # and also recursive models
     os.environ['MINISCOPE_IO_LOGS__LEVEL'] = 'error'
 
     config = Config()
-    assert config.base_dir == Path(tmpdir)
+    assert config.base_dir == Path(tmp_path)
     assert config.log_dir == override_logdir
     assert config.logs.level == 'error'.upper()
     del os.environ['MINISCOPE_IO_BASE_DIR']
@@ -32,16 +32,16 @@ def test_config_from_environment(tmpdir):
     del os.environ['MINISCOPE_IO_LOGS__LEVEL']
 
 
-def test_config_from_dotenv(tmpdir):
+def test_config_from_dotenv(tmp_path):
     """
     dotenv files should also set config
 
     this test can be more relaxed since its basically a repetition of previous
     """
 
-    dotenv = Path(tmpdir) / '.env'
+    dotenv = Path(tmp_path) / '.env'
     with open(dotenv, 'w') as denvfile:
-        denvfile.write(f'MINISCOPE_IO_BASE_DIR="{tmpdir}"')
+        denvfile.write(f'MINISCOPE_IO_BASE_DIR="{tmp_path}"')
 
     config = Config(_env_file=dotenv, _env_file_encoding='utf-8')
-    assert config.base_dir == Path(tmpdir)
+    assert config.base_dir == Path(tmp_path)
