f8d55e8ba3be43cd5329fcf2bfcf8bbe9947b201
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-06-25 19:38:23 -0700

    mark skip

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,7 +2,7 @@ import pytest
 from click.testing import CliRunner
 
 
-@pytest.skip("Needs to be implemented")
+@pytest.mark.skip("Needs to be implemented")
 def test_cli_stream():
     """should be able to invoke streamdaq, using various capture options"""
     pass
