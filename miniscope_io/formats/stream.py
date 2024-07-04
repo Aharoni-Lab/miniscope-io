3cc16d0acf630266b5f36e37c24779229aae6a38
t-sasatani <33111879+t-sasatani@users.noreply.github.com>
2024-07-04 19:43:14 +0900

    add missing elements and notes to streamdaq header format

--- miniscope_io/formats/stream.py
+++ miniscope_io/formats/stream.py
@@ -1,5 +1,6 @@
 """
 Formats for use with :mod:`miniscope_io.stream_daq`
+We plan to re-define this soon so documentation will come after that.
 """
 
 from miniscope_io.models.stream import StreamBufferHeaderFormat
@@ -9,6 +10,11 @@ StreamBufferHeader = StreamBufferHeaderFormat(
     frame_num=1,
     buffer_count=2,
     frame_buffer_count=3,
+    write_buffer_count=4,
+    dropped_buffer_count=5,
     timestamp=6,
     pixel_count=7,
+    write_timestamp=8,
+    battery_voltage=9,
+    ewl_pos=10,
 )
