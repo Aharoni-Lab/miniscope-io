b0f39badd647990c4e8068ce88a6fbe79f83bce6
t-sasatani <33111879+t-sasatani@users.noreply.github.com>
2024-11-01 18:54:06 +0900

    Isolate models in a separate file

--- miniscope_io/cli/update.py
+++ miniscope_io/cli/update.py
@@ -4,7 +4,8 @@ CLI for updating device over IR or UART.
 
 import click
 
-from miniscope_io.device_update import DevUpdate, DeviceCommand
+from miniscope_io.device_update import DeviceCommand, DevUpdate
+
 
 @click.command()
 @click.option(
@@ -39,7 +40,7 @@ from miniscope_io.device_update import DevUpdate, DeviceCommand
     "--restart",
     is_flag=True,
     type=bool,
-    help="Restart the device. Cannot be used with --target or --value."
+    help="Restart the device. Cannot be used with --target or --value.",
 )
 def update(port: str, target: str, value: int, device_id: int, restart: bool) -> None:
     """
@@ -49,17 +50,13 @@ def update(port: str, target: str, value: int, device_id: int, restart: bool) ->
     # Check mutual exclusivity
     if (target and not value) or (value and not target):
         raise click.UsageError("Both --target and --value are required if one is specified.")
-    
+
     if (target or value) and restart:
         raise click.UsageError("Options --target/--value and --restart cannot be used together.")
 
     if target and value:
         DevUpdate(port=port, target=target, value=value, device_id=device_id)
     elif restart:
-        DevUpdate(port=port,
-                  target='DEVICE',
-                  value=DeviceCommand.RESTART,
-                  device_id=device_id
-                  )
+        DevUpdate(port=port, target="DEVICE", value=DeviceCommand.RESTART, device_id=device_id)
     else:
         raise click.UsageError("Either --target with --value or --restart must be specified.")
---
+++ miniscope_io/models/devupdate.py
@@ -0,0 +1,105 @@
+"""
+Models for device update commands.
+"""
+
+from enum import Enum
+
+import serial.tools.list_ports
+from pydantic import BaseModel, ConfigDict, field_validator, model_validator
+
+
+class DeviceCommand(Enum):
+    """Commands for device."""
+
+    RESTART = 200
+
+
+class UpdateTarget(Enum):
+    """Targets to update."""
+
+    LED = 0
+    GAIN = 1
+    ROI_X = 2
+    ROI_Y = 3
+    ROI_WIDTH = 4  # not implemented
+    ROI_HEIGHT = 5  # not implemented
+    EWL = 6  # not implemented
+    DEVICE = 99  # for device commands
+
+
+class DevUpdateCommand(BaseModel):
+    """
+    Command to update device configuration.
+    """
+
+    device_id: int
+    port: str
+    target: UpdateTarget
+    value: int
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    @model_validator(mode="after")
+    def validate_values(cls, values: dict) -> dict:
+        """
+        Validate values based on target.
+        """
+        target = values.target
+        value = values.value
+
+        if target == UpdateTarget.LED:
+            assert 0 <= value <= 100, "For LED, value must be between 0 and 100"
+        elif target == UpdateTarget.GAIN:
+            assert value in [1, 2, 4], "For GAIN, value must be 1, 2, or 4"
+        elif target == UpdateTarget.DEVICE:
+            assert value in DeviceCommand.__members__.values()
+        elif (
+            target == UpdateTarget.ROI_X
+            or target == UpdateTarget.ROI_Y
+            or target == UpdateTarget.ROI_WIDTH
+            or target == UpdateTarget.ROI_HEIGHT
+            or target == UpdateTarget.EWL
+        ):
+            pass  # Need to implement
+        return values
+
+    @field_validator("port")
+    def validate_port(cls, value: str) -> str:
+        """
+        Validate port.
+
+        Args:
+            value: Port to validate.
+
+        Returns:
+            Validated port.
+
+        Raises:
+            ValueError: If no serial ports found or port not found.
+        """
+        portlist = list(serial.tools.list_ports.comports())
+
+        if len(portlist) == 0:
+            raise ValueError("No serial ports found")
+        if value not in [port.device for port in portlist]:
+            raise ValueError(f"Port {value} not found")
+        return value
+
+    @field_validator("target", mode="before")
+    def validate_target(cls, value: str) -> UpdateTarget:
+        """
+        Validate and convert target string to UpdateTarget Enum type.
+
+        Args:
+            value (str): Target to validate.
+
+        Returns:
+            UpdateTarget: Validated target as UpdateTarget.
+
+        Raises:
+            ValueError: If target not found.
+        """
+        try:
+            return UpdateTarget[value]
+        except KeyError as e:
+            raise ValueError(f"Target {value} not found.") from e
