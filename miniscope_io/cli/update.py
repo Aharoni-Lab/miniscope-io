82c3ff7b8e14d5fbe12ede596e4baa25d7c97abf
t-sasatani <33111879+t-sasatani@users.noreply.github.com>
2024-11-01 18:40:09 +0900

    Add device commands (mainly for restart)

--- miniscope_io/cli/update.py
+++ miniscope_io/cli/update.py
@@ -4,8 +4,7 @@ CLI for updating device over IR or UART.
 
 import click
 
-from miniscope_io.device_update import DevUpdate
-
+from miniscope_io.device_update import DevUpdate, DeviceCommand
 
 @click.command()
 @click.option(
@@ -25,13 +24,42 @@ from miniscope_io.device_update import DevUpdate
 @click.option(
     "-t",
     "--target",
-    required=True,
+    required=False,
     type=click.Choice(["LED", "GAIN", "ROI_X", "ROI_Y"]),
-    help="Target to update",
+    help="Target to update. Cannot be used with --restart.",
 )
-@click.option("-v", "--value", required=True, type=int, help="Value to set")
-def update(port: str, target: str, value: int, device_id: int) -> None:
+@click.option(
+    "-v",
+    "--value",
+    required=False,
+    type=int,
+    help="Value to set. Must be used with --target and cannot be used with --restart.",
+)
+@click.option(
+    "--restart",
+    is_flag=True,
+    type=bool,
+    help="Restart the device. Cannot be used with --target or --value."
+)
+def update(port: str, target: str, value: int, device_id: int, restart: bool) -> None:
     """
-    Update device configuration.
+    Update device configuration or restart it.
     """
-    DevUpdate(port=port, target=target, value=value, device_id=device_id)
+
+    # Check mutual exclusivity
+    if (target and not value) or (value and not target):
+        raise click.UsageError("Both --target and --value are required if one is specified.")
+    
+    if (target or value) and restart:
+        raise click.UsageError("Options --target/--value and --restart cannot be used together.")
+
+    if target and value:
+        DevUpdate(port=port, target=target, value=value, device_id=device_id)
+    elif restart:
+        DevUpdate(port=port,
+                  target='DEVICE',
+                  value=DeviceCommand.RESTART,
+                  device_id=device_id
+                  )
+    else:
+        raise click.UsageError("Either --target with --value or --restart must be specified.")
