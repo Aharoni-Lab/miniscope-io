9e315ac296f641318430bd44ba067d2a5551f126
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-11-07 17:54:18 -0800

    rm global import of wirefree

--- miniscope_io/devices/__init__.py
+++ miniscope_io/devices/__init__.py
@@ -4,5 +4,6 @@ Devices!
 
 from miniscope_io.devices.camera import Camera, Miniscope
 from miniscope_io.devices.device import Device
+from miniscope_io.devices.wirefree import WireFreeMiniscope
 
-__all__ = ["Camera", "Device", "Miniscope"]
+__all__ = ["Camera", "Device", "Miniscope", "WireFreeMiniscope"]
