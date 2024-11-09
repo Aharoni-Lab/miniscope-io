9907e22c32f2582fcbf3e2cf87bf1b36ada8deba
t-sasatani <33111879+t-sasatani@users.noreply.github.com>
2024-11-09 22:29:25 +0900

    Test log level setting from dotenv

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,5 +1,6 @@
 import pdb
 
+import logging
 import pytest
 import os
 import tempfile
@@ -43,4 +44,24 @@ def test_init_logger(capsys, tmp_path):
         log_str = lfile.read()
     assert 'INFO' not in log_str
 
+@pytest.mark.parametrize('level', ['DEBUG', 'INFO', 'WARNING', 'ERROR'])
+def test_init_logger_from_dotenv(tmp_path, level):
+    """
+    Set log levels from dotenv MINISCOPE_IO_LOGS__LEVEL key
+    """
+    # Feels kind of fragile to hardcode this but I couldn't think of a better way so for now
+    level_name_map = {
+        'DEBUG': logging.DEBUG,
+        'INFO': logging.INFO,
+        'WARNING': logging.WARNING,
+        'ERROR': logging.ERROR
+    }
 
+    tmp_path.mkdir(exist_ok=True,parents=True)
+    dotenv = tmp_path / '.env'
+    with open(dotenv, 'w') as denvfile:
+        denvfile.write(f'MINISCOPE_IO_LOGS__LEVEL="{level}"')
+    
+    dotenv_logger = init_logger(name='test_logger', log_dir=tmp_path)
+
+    assert dotenv_logger.level == level_name_map.get(level)
