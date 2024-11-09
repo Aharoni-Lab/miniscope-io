4f946909ac572a9b3bf6d57514f7eefde40c70a7
t-sasatani <33111879+t-sasatani@users.noreply.github.com>
2024-11-09 22:06:17 +0900

    Use project log level in device update

--- miniscope_io/device_update.py
+++ miniscope_io/device_update.py
@@ -11,7 +11,7 @@ import serial.tools.list_ports
 from miniscope_io.logging import init_logger
 from miniscope_io.models.devupdate import DevUpdateCommand, UpdateCommandDefinitions
 
-logger = init_logger(name="device_update", level="INFO")
+logger = init_logger(name="device_update")
 FTDI_VENDOR_ID = 0x0403
 FTDI_PRODUCT_ID = 0x6001
 
