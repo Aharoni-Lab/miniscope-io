8333a5f5922b15eebc493f19e1e4809d5ab0ab7e
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-11-15 21:17:53 -0800

    rm formats module

--- miniscope_io/formats/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Instantiations of :class:`~.miniscope_io.models.MiniscopeConfig` models
-that describe fixed per-device configurations for the generic config
-models in :mod:`~.miniscope_io.models.stream` et al.
-"""
