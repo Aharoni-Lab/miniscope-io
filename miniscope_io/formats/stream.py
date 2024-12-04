4ceec64f98be3d6cfc61190757be019a2dde21bc
hsemwal <80985665+hsemwal@users.noreply.github.com>
2024-12-03 16:06:02 -0800

    MSUS Io - trying to change the config file to match the firmware - generalize the config file and stream_daq.py

--- miniscope_io/formats/stream.py
+++ miniscope_io/formats/stream.py
@@ -4,7 +4,7 @@ We plan to re-define this soon so documentation will come after that.
 """
 
 from miniscope_io.models.stream import StreamBufferHeaderFormat
-
+""" 
 StreamBufferHeader = StreamBufferHeaderFormat(
     linked_list=0,
     frame_num=1,
@@ -17,4 +17,19 @@ StreamBufferHeader = StreamBufferHeaderFormat(
     write_timestamp=8,
     battery_voltage_raw=9,
     input_voltage_raw=10,
+)
+"""
+
+StreamBufferHeader = StreamBufferHeaderFormat(
+    linked_list=2,
+    frame_num=3,
+    buffer_count=5,
+    frame_buffer_count=4,
+    write_buffer_count=6,
+    dropped_buffer_count=7,
+    timestamp=8,
+    pixel_count=3,
+    write_timestamp=8,
+    battery_voltage_raw=10,
+    input_voltage_raw=11,
 )
